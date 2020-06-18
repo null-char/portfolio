20d3c79e2c9ee15349fd43504a1e69c1db369dc8
SKP <skp.frl@gmail.com>
2020-06-18 22:20:46 +0530

    feat(hooks): make useScrollAnimation more configurable

--- src/hooks/useScrollAnimation.ts
+++ src/hooks/useScrollAnimation.ts
@@ -1,14 +1,30 @@
 import { useEffect } from "react"
-import { useInView } from "react-intersection-observer"
-import { useAnimation, Variants, Orchestration, Spring } from "framer-motion"
+import { useInView, IntersectionOptions } from "react-intersection-observer"
+import {
+  useAnimation,
+  Variants,
+  Orchestration,
+  Spring,
+  Tween,
+  AnimationControls,
+  Transition,
+} from "framer-motion"
 
-type AnimationTypes = "fade" | "slide" | "skewedSlide"
+type AnimationTypes = "fade" | "slide"
+type Options = {
+  animation?: Orchestration & Partial<Spring | Tween>
+  intersection?: IntersectionOptions
+  inViewCallback?: () => void
+}
+type VariantStates = "visible" | "hidden"
 
 /**
  * A hook which handles animation logic for scroll animations. Uses framer motion's `useAnimation` hook internally. Uses a "spring" animation.
  *
  * @param type Type of animation to play. Set to "fade" by default.
  * @param options Provide animation transition options.
+ * @param intersectionOption Provide options for intersection observer
+ * @param inViewCallback A callback that will be called whenever the observed node goes out of view or comes into view
  *
  * @returns An object containing `ref`, `controls` and `variants`
  * Provide ref to the motion component to be scroll animated. Provide controls and variants to the parent and optionally its children (also must be motion components) if you wish to do stagger animations. The `staggerChildren` transiton property is set to 0.1 seconds by default.
@@ -16,19 +32,32 @@ type AnimationTypes = "fade" | "slide" | "skewedSlide"
  */
 export const useScrollAnimation = (
   type?: AnimationTypes,
-  options?: Orchestration & Partial<Spring>
+  options?: Options
 ) => {
   const animation = type || "fade"
+  const intersectionOptions = options?.intersection || {}
+  const animationOptions = options?.animation || {}
 
-  const [ref, inView] = useInView()
+  const [ref, inView] = useInView({ ...intersectionOptions })
   const controls = useAnimation()
-  controls.setDefaultTransition({
-    type: "spring",
-    stiffness: 30,
-    mass: 0.3,
-    staggerChildren: 0.1,
-    ...options,
-  })
+
+  // fade transitions work better with tween
+  if (animation === "fade") {
+    controls.setDefaultTransition({
+      type: "tween",
+      duration: 0.1,
+      staggerChildren: 0.1,
+      ...animationOptions,
+    })
+  } else {
+    controls.setDefaultTransition({
+      type: "spring",
+      stiffness: 30,
+      mass: 0.3,
+      staggerChildren: 0.1,
+      ...options,
+    })
+  }
 
   const allVariants: { [key in AnimationTypes]: Variants } = {
     fade: {
@@ -49,33 +78,35 @@ export const useScrollAnimation = (
         translateX: "-100%",
       },
     },
-    skewedSlide: {
-      visible: {
-        opacity: 1,
-        translateX: "0%",
-        skewX: "0deg",
-      },
-      hidden: {
-        opacity: 0,
-        translateX: "-100%",
-        skewX: "45deg",
-      },
-    },
   }
 
   useEffect(() => {
-    if (inView) {
-      controls.start("visible")
+    // use the provided callback if it is provided else default to basic controls
+    if (options?.inViewCallback) {
+      options.inViewCallback()
     } else {
-      controls.start("hidden")
+      if (inView) {
+        controls.start("visible")
+      } else {
+        controls.start("hidden")
+      }
     }
-  }, [controls, inView])
+  }, [controls, inView, options])
 
   const variants = allVariants[animation]
+  // restrict controls
+  controls as AnimationControls & {
+    start: (
+      definition: VariantStates,
+      transitionOverrides: Transition
+    ) => Promise<any>
+    set: (definition: VariantStates) => void
+  }
 
   return {
     ref,
     controls,
     variants,
+    inView,
   }
 }
