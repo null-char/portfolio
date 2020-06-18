8ca6226e9e4e9b6c532e06f12db484f190d68646
SKP <skp.frl@gmail.com>
2020-06-18 22:19:23 +0530

    feat(utils): add a media screen breakpoint for large tablets

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -3,21 +3,23 @@ export const size = {
   mobileM: "375px",
   mobileL: "425px",
   tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
+  tabletL: "1024px",
+  laptop: "1336px",
+  laptopL: "1600px",
   desktop: "1920px",
   desktopL: "2560px",
-};
+}
 
 const device = {
   mobileS: `screen and (min-width: ${size.mobileS})`,
   mobileM: `screen and (min-width: ${size.mobileM})`,
   mobileL: `screen and (min-width: ${size.mobileL})`,
   tablet: `screen and (min-width: ${size.tablet})`,
+  tabletL: `screen and (min-width: ${size.tabletL})`,
   laptop: `screen and (min-width: ${size.laptop})`,
   laptopL: `screen and (min-width: ${size.laptopL})`,
   desktop: `screen and (min-width: ${size.desktop})`,
   desktopL: `screen and (min-width: ${size.desktopL})`,
-};
+}
 
-export default device;
+export default device
