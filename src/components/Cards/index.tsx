b95a70642674e941b51cd1f024a8453927f842a3
SKP <skp.frl@gmail.com>
2020-06-18 22:20:00 +0530

    feat: add Cards component

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,95 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { useScrollAnimation } from "@/hooks/useScrollAnimation"
+import {
+  Wrapper,
+  CenteredHeading,
+  CardsGrid,
+  Card,
+  CardMain,
+  CardText,
+  SVGImg,
+  ColoredText,
+} from "@/components/Cards/styles"
+
+type QueryData = {
+  allCardsYaml: {
+    edges: {
+      node: {
+        feat: string
+        description: string
+        svg: {
+          publicURL: string
+        }
+      }
+    }[]
+  }
+}
+
+const Cards: React.FC = () => {
+  const data: QueryData = useStaticQuery(graphql`
+    query {
+      allCardsYaml {
+        edges {
+          node {
+            feat
+            description
+            svg {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const {
+    allCardsYaml: { edges: cards },
+  } = data
+
+  const {
+    ref: cRef,
+    controls: cControls,
+    variants: cVariants,
+    inView: cInView,
+  } = useScrollAnimation("fade")
+
+  // overrides the useEffect callback to only hide the heading if the Cards' inView state is false
+  const {
+    ref: hRef,
+    controls: hControls,
+    variants: hVariants,
+    inView: hInView,
+  } = useScrollAnimation("slide", {
+    inViewCallback: () => {
+      if (hInView) {
+        hControls.start("visible")
+      } else if (!cInView) {
+        hControls.start("hidden")
+      }
+    },
+  })
+
+  return (
+    <Wrapper>
+      <CenteredHeading ref={hRef} animate={hControls} variants={hVariants}>
+        My Approach
+      </CenteredHeading>
+
+      <CardsGrid ref={cRef} animate={cControls} variants={cVariants}>
+        {cards.map(({ node: card }) => (
+          <Card animate={cControls} variants={cVariants}>
+            <CardMain>
+              <SVGImg src={card.svg.publicURL} alt="card svg" />
+              <ColoredText>{card.feat}</ColoredText>
+            </CardMain>
+
+            <CardText>{card.description}</CardText>
+          </Card>
+        ))}
+      </CardsGrid>
+    </Wrapper>
+  )
+}
+
+export default Cards
---
+++ src/components/Cards/styles.ts
@@ -0,0 +1,107 @@
+import styled from "styled-components"
+import device from "@/utils/media"
+import Paragraph from "@/components/Paragraph"
+import { motion } from "framer-motion"
+
+export const Wrapper = styled.section`
+  display: grid;
+  grid-template-rows: repeat(2, min-content);
+  row-gap: 4rem;
+  padding: 0rem 3rem;
+  margin-top: 5rem;
+
+  @media ${device.tabletL} {
+    padding: 0rem 5rem;
+  }
+
+  @media ${device.laptop} {
+    padding: 0rem 7rem;
+  }
+
+  @media ${device.laptopL} {
+    padding: 0rem 10rem;
+  }
+
+  padding-bottom: 20rem !important; /* temporary padding */
+`
+
+export const CardsGrid = styled(motion.div)`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  row-gap: 4rem;
+  justify-items: center;
+  justify-self: center;
+
+  @media ${device.tablet} {
+    grid-template-columns: 35rem;
+  }
+
+  @media ${device.tabletL} {
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+
+  @media ${device.laptop} {
+    column-gap: 5rem;
+  }
+`
+
+export const Card = styled(motion.div)`
+  display: grid;
+  grid-template-rows: repeat(2, min-content);
+  width: 100%;
+  row-gap: 1.5rem;
+  justify-items: center;
+  padding: 3rem 0rem;
+  background-color: ${props => props.theme.colors.cardBackground};
+  border-radius: 0.5rem;
+  transition: all 0.3s ease-in-out;
+
+  :hover {
+    transform: translateY(-0.2rem);
+  }
+`
+
+export const CardMain = styled(motion.div)`
+  display: grid;
+  grid-template-rows: repeat(2, min-content);
+  row-gap: 2rem;
+  justify-items: center;
+`
+
+export const SVGImg = styled.img`
+  width: 7rem;
+  height: 7rem;
+`
+
+export const ColoredText = styled.h3`
+  color: ${props => props.theme.colors.primary};
+  font-size: 1.6rem;
+  text-transform: none;
+`
+
+export const CenteredHeading = styled(motion.h2)`
+  font-size: 2.7rem;
+  font-weight: 700;
+  font-family: Fira Sans, sans-serif;
+  text-align: center;
+  text-transform: uppercase;
+  color: ${props => props.theme.colors.text};
+
+  @media ${device.tablet} {
+    font-size: 3.7rem;
+  }
+`
+
+export const CardText = styled(motion.p)`
+  line-height: 1.45;
+  color: ${props => props.theme.colors.text};
+  font-size: 1.6rem;
+  text-align: center;
+  padding: 0rem 2rem;
+
+  @media ${device.laptop} {
+    font-size: 1.5rem;
+  }
+`
