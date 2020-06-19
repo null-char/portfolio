b6f66400e5e87d2f5d78ff221642a0d728f7bfa0
SKP <skp.frl@gmail.com>
2020-06-19 11:09:47 +0530

    chore: add icons to manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,48 @@ module.exports = {
         start_url: `/`,
         background_color: `#00080A`,
         theme_color: `#0EDEFF`,
-        display: `minimal-ui`,
+        display: `standalone`,
+        "icons": [{
+            "src": "src/assets/icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
       },
     },
     {
