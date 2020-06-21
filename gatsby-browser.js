e638d13e1f7b60c4499f012419ca6dd81dd7d119
SKP <skp.frl@gmail.com>
2020-06-21 20:56:26 +0530

    fix: disable unnecessary horizontal scroll on mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,34 @@
 require("typeface-montserrat")
 require("typeface-fira-sans")
+const { default: Scrollbar, ScrollbarPlugin } = require("smooth-scrollbar")
+
+class DisableScrollPlugin extends ScrollbarPlugin {
+  static pluginName = "disableScroll"
+
+  static defaultOptions = {
+    direction: null,
+  }
+
+  transformDelta(delta) {
+    if (this.options.direction) {
+      delta[this.options.direction] = 0
+    }
+
+    return { ...delta }
+  }
+}
+Scrollbar.use(DisableScrollPlugin)
 
 // right after React.render is called
 exports.onRouteUpdate = () => {
-  // only init smooth scroll for desktop devices
-  if (window.screen.width > 1024) {
-    const {
-      default: Scrollbar
-    } = require("smooth-scrollbar")
-
-    // look for something with the id of smooth-scroll
-    //! won't work unless the scrollable area is specified explicitly for each page
-    Scrollbar.init(document.querySelector("#smooth-scroll"))
-  }
+  // look for something with the id of smooth-scroll
+  //! won't work unless the scrollable area is specified explicitly for each page
+  const scroll = Scrollbar.init(document.querySelector("#smooth-scroll"), {
+    plugins: {
+      disableScroll: {
+        direction: "x",
+      },
+    },
+  })
+  scroll.track.xAxis.element.remove()
 }
