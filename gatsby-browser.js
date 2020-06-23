c2ef9031c9bc47d6ac00b5442ff05bafe8833edb
SKP <skp.frl@gmail.com>
2020-06-23 13:38:07 +0530

    refactor: change to es6 imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -24,7 +24,7 @@ class DisableScrollPlugin extends ScrollbarPlugin {
 Scrollbar.use(DisableScrollPlugin)
 
 // right after React.render is called
-exports.onRouteUpdate = () => {
+export const onRouteUpdate = () => {
   // look for something with the id of smooth-scroll
   //! won't work unless the scrollable area is specified explicitly for each page
   const scroll = Scrollbar.init(document.querySelector("#smooth-scroll"), {
